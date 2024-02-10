5dbb613a777b70fa62ed0d102aeeae99b438d74a
Misha139139 <157865174+Misha139139@users.noreply.github.com>
2024-02-10 14:55:56 +1000

    Add files via upload

---
+++ Assets/move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 0.5f;
+    private Vector2 moveVector;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground") {
+            Debug.Log("Столкнулись");
+        }
+    }
+}
